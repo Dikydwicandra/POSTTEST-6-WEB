a91e6a01791af34b5ebb55c7eb342329eaa08a1b
Dikydwicandra <113652422+Dikydwicandra@users.noreply.github.com>
2022-10-30 16:08:37 +0800

    Add

---
+++ data_film.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2022 at 09:00 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `datafilm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_film`
+--
+
+CREATE TABLE `data_film` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `tahun` int(10) NOT NULL,
+  `genre` varchar(25) NOT NULL,
+  `durasi` int(10) NOT NULL,
+  `rating` int(10) NOT NULL,
+  `upload` varchar(255) NOT NULL,
+  `keterangan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_film`
+--
+ALTER TABLE `data_film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_film`
+--
+ALTER TABLE `data_film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
